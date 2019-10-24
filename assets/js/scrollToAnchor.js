82ab04dd0cf6e268ad1ec5677e0193f7a379b797
jag19 <56983865+jag19@users.noreply.github.com>
2019-10-24 19:49:06 -0400

    Upload

---
+++ assets/js/scrollToAnchor.js
@@ -0,0 +1,8 @@
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+$("#statementsButton").click(function() {
+	   scrollToAnchor('statements');
+});
